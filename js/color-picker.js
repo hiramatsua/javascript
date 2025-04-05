363614f658026ab94dde97d01a99202ec12be080
Akihisa Hiramatsu <akihisa_h@icloud.com>
2025-04-05 15:36:53 +0900

    step3 カラーピッカー変更されたらカラーコードを表示する

--- js/color-picker.js
+++ js/color-picker.js
@@ -1,11 +1,15 @@
 /*
     for color-picker jsファイル
 */
-const colorCode = document.querySelector('#colorPicker');
+const colorPic = document.querySelector('#colorPicker');
 const colorText = document.querySelector('#colorText');
 
 alert('color-picker.js 起動');
-console.log(colorCode.value);
 
-// テキスト内容を書き換え
-colorText.textContent = 'カラーコード：';
+function colorBg () {
+    // テキスト内容を書き換え
+    colorText.textContent = 'カラーコード：' + colorPic.value;
+}
+
+// カラーピッカーが変更されたら、function colorBg() 実行
+colorPic.addEventListener('input', colorBg);
