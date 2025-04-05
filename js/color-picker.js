7bf0999cb3bb51bdd33cd34ee2ff104007c89940
Akihisa Hiramatsu <akihisa_h@icloud.com>
2025-04-05 11:43:06 +0900

    step2-coding-test

--- js/color-picker.js
+++ js/color-picker.js
@@ -1,1 +1,11 @@
+/*
+    for color-picker jsファイル
+*/
+const colorCode = document.querySelector('#colorPicker');
+const colorText = document.querySelector('#colorText');
+
 alert('color-picker.js 起動');
+console.log(colorCode.value);
+
+// テキスト内容を書き換え
+colorText.textContent = 'カラーコード：';
