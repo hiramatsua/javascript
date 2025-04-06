1dec36282aacabb07fce613afe81c0b33f2319ab
Akihisa Hiramatsu <akihisa_h@icloud.com>
2025-04-06 15:54:03 +0900

    classlist-toggle -step2- モード状態によってボタン名を切り替える

--- js/classlist-toggle.js
+++ js/classlist-toggle.js
@@ -7,6 +7,13 @@ function modeChange () {
 
     // body にclass="dark-mode" を付けたり外したりする
     document.body.classList.toggle('dark-mode');
+
+    // ボタン記述を変化
+    if (btn.textContent == 'ダークモードに切り替える') {
+        btn.textContent = '通常モードに切り替える';
+    } else {
+        btn.textContent = 'ダークモードに切り替える';
+    }
 }
 
 // ボタンがクリックされたら、画面モードを切り替える
